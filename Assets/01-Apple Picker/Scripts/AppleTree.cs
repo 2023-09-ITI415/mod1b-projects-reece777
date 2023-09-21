264721ef9f98ded305bcfe82bdeb991cf2750eb1
reece777 <44884218+reece777@users.noreply.github.com>
2023-09-21 17:02:37 -0400

    Created physics layers for apples, appletree, and basket

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -21,8 +21,18 @@ public class AppleTree : MonoBehaviour
     void Start()
     {
         //Dropping apples every second
+
+        Invoke("DropApple", 2f);
     }
 
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+
+        apple.transform.position = transform.position;
+
+        Invoke("DropApple", secondsBetweenAppleDrop);
+    }
     // Update is called once per frame
     void Update()
     {
