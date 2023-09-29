7ef2c9dd2564260308a87caaab9b3362d700829c
reece777 <44884218+reece777@users.noreply.github.com>
2023-09-29 18:58:05 -0400

    Launchpoint halo sets active on mouse enter

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject launchPoint;
+
+     void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint"); //a
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+    void OnMouseEnter()
+    {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+     void OnMouseExit()
+    {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+}
