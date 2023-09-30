ace98a6478d861f8b85f12ee180364b97a8a686c
reece777 <44884218+reece777@users.noreply.github.com>
2023-09-30 12:47:19 -0400

    Camera is less janky and expands to  encapsulate ground

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -10,6 +10,9 @@ public class FollowCam : MonoBehaviour
     //the desired z pos of camera
     public float camZ;
 
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
      void Awake()
     {
         camZ = this.transform.position.z;
@@ -22,10 +25,22 @@ public class FollowCam : MonoBehaviour
 
         //get the position of the POI
         Vector3 destination = POI.transform.position;
+
+        //limit the x and y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
+        //interpolate from the current camera pos toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+
         //sets destination.z to be camZ to keep camera far enough away
         destination.z = camZ;
+
         //set the camera to the destination
         transform.position = destination;
+
+        //set the orthographicSize or the camera to keep Ground in view
+        Camera.main.orthographicSize = destination.y + 10;
     }
 
 }
