76e88afe685bab9c9299511e0fefa90034ab2187
reece777 <44884218+reece777@users.noreply.github.com>
2023-10-09 19:45:39 -0400

    Created camera and player controller

--- Assets/03-Prototype1/Scripts/PlayerController.cs
+++ Assets/03-Prototype1/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
+    
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed;
+        rb.velocity = movement;
     }
 }
